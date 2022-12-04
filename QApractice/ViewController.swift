1be689ae713ef25dd9818472d7bff752f893459b
“silverspring2y“ <silverspring.2y@gmail.com>
2022-12-04 12:43:30 +0900

    Initial Commit

---
+++ QApractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QApractice
+//
+//  Created by Shinichiro Tanaka on 2022/12/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
